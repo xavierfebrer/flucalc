22666e79a04c9b3894b24ceda1fa62a741aefa06
Xavier Febrer Jordà <xavierfebrer@gmail.com>
2021-08-12 00:55:19 +0200

    Add main integration to sdk.

--- flucalc/lib/main.dart
+++ flucalc/lib/main.dart
@@ -2,17 +2,65 @@ import 'package:flucalc/util/constant.dart';
 import 'package:flucalc/view/screen/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hack2s_flutter_util/util/app_data_provider.dart';
 import 'package:hack2s_flutter_util/util/app_util.dart';
 import 'package:hack2s_flutter_util/view/app/base_app.dart';
 
-void main() {
-  Hack2sAppUtil.runApplication([DeviceOrientation.portraitUp], () => FlucalcApp());
+import 'di/locator.dart';
+
+Future<void> main() async {
+  await Hack2sAppUtil.runApplication(DeviceOrientation.values, FlucalcAppDataProvider(), () async {
+    return await Future(() async {
+      await Locator.initializeDI();
+      return FlucalcApp();
+    });
+  });
 }
 
 class FlucalcApp extends BaseApp {
   FlucalcApp({Key? key}) : super(key: key);
 
   @override
-  Widget getWidget() => Hack2sAppUtil.getDefaultMaterialApp(
-      FlucalcConstant.APP_NAME, Hack2sAppUtil.getDefaultThemeData(FlucalcConstant.COLOR_PRIMARY, FlucalcConstant.COLOR_ACCENT), HomeScreen());
+  Widget getWidget() => Hack2sAppUtil.getDefaultMaterialApp(HomeScreen());
+}
+
+class FlucalcAppDataProvider extends Hack2sAppDataProvider {
+  @override
+  String get APP_NAME => FlucalcConstant.APP_NAME;
+
+  @override
+  Color get COLOR_PRIMARY => FlucalcConstant.COLOR_PRIMARY;
+
+  @override
+  Color get COLOR_SECONDARY => FlucalcConstant.COLOR_SECONDARY;
+
+  @override
+  Color COLOR_TEXT([bool darkMode = false]) => FlucalcConstant.COLOR_TEXT(darkMode);
+
+  @override
+  Color COLOR_TEXT_2([bool darkMode = false]) => FlucalcConstant.COLOR_TEXT_2(darkMode);
+
+  @override
+  FontWeight get TEXT_FONT_WEIGHT_LIGHT => FlucalcConstant.TEXT_FONT_WEIGHT_LIGHT;
+
+  @override
+  FontWeight get TEXT_FONT_WEIGHT => FlucalcConstant.TEXT_FONT_WEIGHT;
+
+  @override
+  FontWeight get TEXT_FONT_WEIGHT_MEDIUM => FlucalcConstant.TEXT_FONT_WEIGHT_MEDIUM;
+
+  @override
+  FontWeight get TEXT_FONT_WEIGHT_SEMI_BOLD => FlucalcConstant.TEXT_FONT_WEIGHT_SEMI_BOLD;
+
+  @override
+  FontWeight get TEXT_FONT_WEIGHT_BOLD => FlucalcConstant.TEXT_FONT_WEIGHT_BOLD;
+
+  @override
+  double get TEXT_LETTER_SPACING => FlucalcConstant.TEXT_LETTER_SPACING;
+
+  @override
+  double get TEXT_PRIMARY_FONT_SIZE => FlucalcConstant.TEXT_PRIMARY_FONT_SIZE;
+
+  @override
+  double get TEXT_SECONDARY_FONT_SIZE => FlucalcConstant.TEXT_SECONDARY_FONT_SIZE;
 }
--- flucalc/lib/util/constant.dart
+++ flucalc/lib/util/constant.dart
@@ -3,8 +3,25 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class FlucalcConstant {
-  static const String EMPTY_STRING = "";
   static const String APP_NAME = "Flucalc";
+  static const String LOG_TAG = APP_NAME + " Log";
+  static const Color COLOR_PRIMARY = Color(0xFF795548);
+  static const Color COLOR_SECONDARY = Color(0xFFD7CCC8);
+  static Color COLOR_TEXT([bool darkMode = false]) => !darkMode ? Color(0xFF212121) : Color(0xFFFFFFFF);
+  static Color COLOR_TEXT_2([bool darkMode = false]) => !darkMode ? Color(0xFF757575) : Color(0xFFD1D1D1);
+  static const double TEXT_PRIMARY_FONT_SIZE = 18;
+  static const double TEXT_SECONDARY_FONT_SIZE = 16;
+  static const FontWeight TEXT_FONT_WEIGHT_LIGHT = FontWeight.w300;
+  static const FontWeight TEXT_FONT_WEIGHT = FontWeight.w400;
+  static const FontWeight TEXT_FONT_WEIGHT_MEDIUM = FontWeight.w500;
+  static const FontWeight TEXT_FONT_WEIGHT_SEMI_BOLD = FontWeight.w600;
+  static const FontWeight TEXT_FONT_WEIGHT_BOLD = FontWeight.w700;
+  static const double TEXT_LETTER_SPACING = -0.0;
+
+
+
+
+  static const String EMPTY_STRING = "";
   static const int DELAY_SPLASH_SCREEN = 1600;
   static const String DECIMAL_SEPARATOR = ".";
   static const int INTEGER_STARTING_OPTIONAL = 0;
@@ -29,9 +46,7 @@ class FlucalcConstant {
   static const String SYMBOL_INVERT = "+/-";
   static const String SYMBOL_CLEAR = "C";
   static const String SYMBOL_BACK = "<";
-  static const Color COLOR_PRIMARY = Color(0xFF795548);
   static const Color COLOR_DISABLED = Color(0xFFBDBDBD);
-  static const Color COLOR_ACCENT = Color(0xFFD7CCC8);
   static const Color COLOR_EXTRA = Color(0xFF2196F3);
   static const Color COLOR_EXTRA_SELECTED = Color(0xFF1976D2);
   static const Color COLOR_EXTRA_2 = Color(0xFF4CAF50);
